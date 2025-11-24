d2cdeccf06bef5e46295c26eb9f0179fb4314ba0
Bobby Bernarddi Widjayanto <bobbybernarddi0@gmail.com>
2025-11-24 13:41:36 +0700

    Create Karyawan.java

---
+++ latihan/src/Perusahaan/Karyawan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Perusahaan;
+
+/**
+ *
+ * @author Lab Studio
+ */
+public class Karyawan {
+    private String NIP;
+    private String nama;
+    private String divisi;
+
+    public String getNIP() {
+        return NIP;
+    }
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDivisi() {
+        return divisi;
+    }
+
+    public void setDivisi(String divisi) {
+        this.divisi = divisi;
+    }
+}
