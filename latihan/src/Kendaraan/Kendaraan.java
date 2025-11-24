6508346c4a5f689744f2cee26828e5268df941d7
Bobby Bernarddi Widjayanto <bobbybernarddi0@gmail.com>
2025-11-24 13:31:50 +0700

    Kendaraan

---
+++ latihan/src/Kendaraan/Kendaraan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Kendaraan;
+
+/**
+ *
+ * @author Lab Studio
+ */
+public class Kendaraan {
+    private String noPolisi;
+    private String merk;
+    private int tahun;
+
+    public String getNoPolisi() {
+        return noPolisi;
+    }
+
+    public void setNoPolisi(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+}
